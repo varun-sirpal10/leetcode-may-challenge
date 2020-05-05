af783b4f0a7f32e33e1ff79f494a468403db339e
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-05 19:38:02 +0530

    leetcode

---
+++ Week 1/number_complement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        
+        int temp = num;
+        int bit = 1;
+        
+        while(temp!=0){
+            num = num ^ bit;
+            bit = bit << 1;
+            temp = temp >> 1;
+        }
+        
+        return num;
+    }
+};
